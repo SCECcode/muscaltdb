c0de4904f7483a12d9f125dedf482eb3434c0f35
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2026-09-18 05:32:31 +0000

    add tiledb in configure call

--- .github/scripts/muscaltdb-build.sh
+++ .github/scripts/muscaltdb-build.sh
@@ -18,7 +18,7 @@ libtoolize
 aclocal -I m4
 autoconf
 automake --add-missing --force-missing
-./configure --prefix=$UCVM_INSTALL_PATH/model/muscaltdb
+./configure --prefix=$UCVM_INSTALL_PATH/model/muscaltdb --with-tiledb-include-path=$UCVM_INSTALL_PATH/lib/tiledb/include --with-tiledb-lib-path='$UCVM_INSTALL_PATH/lib/tiledb/lib64 -ltiledb -Wl,-rpath,$UCVM_INSTALL_PATH}/lib/tiledb/lib64'
 
 make
 make install
